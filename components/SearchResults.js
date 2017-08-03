05f316b28ca78d3bcfd0abb06b56864221f9c857
mendelB <mendelblesofsky@gmail.com>
2017-08-03 14:35:53 -0400

    Done.

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -3,7 +3,13 @@
 const React = require('react');
 
 const SearchResults = ({ results }) => (
-  <ul>
+  <ul className="search-results">
+  	{results.map((res,i) => 
+  		<li key={i}>
+  			<a href={res.link}>{res.title}</a>
+  			<p>{res.description}</p>
+  		</li>
+  	)}
   </ul>
 );
 
--- stores/resultStore.js
+++ stores/resultStore.js
@@ -2,7 +2,10 @@
 
 const Store = require('./Store');
 
-class ResultStore {
+class ResultStore extends Store {
+	isOutdated(date) {
+		return this.state.updated > date
+	}
 }
 
-module.exports = new ResultStore();
+module.exports = new ResultStore({results: []});
